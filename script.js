9557646b5b6fe21689f154b4fc129c43c59e5fb5
cbw29512 <divclass01@gmail.com>
2021-03-06 13:41:12 -0500

    working and checked iteration 2

--- script.js
+++ script.js
@@ -4,21 +4,34 @@ function checkPassword() {
 
 const pw = document.getElementById("password").value;
 
-if (pw.length < 8) {
-  alert("Password is not accepted");
+if ((pw.length < 8) && (!pw.match(/[a-z]/g) && (/[A-Z]/g))) {
+  alert("Password must be over 8 characters and must include atleast one letter");
+  return false;
+
+} else if ((pw.length < 8) && (!pw.match(/[0-9]/g))) {
+  alert("Password must be over 8 characters and must include atleast one number");
   return false;
 
 } else if (!pw.match(/[a-z]/g) && (/[A-Z]/g)) {
-  alert("Password is not accepted");
+  alert("Password must include atleast one letter");
   return false;
  
 } else if (!pw.match(/[0-9]/g)) {
-  alert("Password is not accepted");
+  alert("Password must include atleast one number");
+  return false;
+
+} else if (!pw.match(/[0-9]/g) && (!pw.match(/[a-z]/g) && (/[A-Z]/g))) {
+  alert("Password must include atleast one number and one number");
+  return false;
+
+} else if (pw.length < 8) {
+  alert("Password must be over 8 characters");
   return false;
 
 } else {
   alert("Password is accepted");
   return true;
 }
+
 }
 
