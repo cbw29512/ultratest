5abf88a1e643c74d86e6a61c13e0f900940a5b37
cbw29512 <divclass01@gmail.com>
2021-03-06 13:22:01 -0500

    working and checked iteration 1

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
+
+
 function checkPassword() {
-  const alphabet =
-    "A, a, B, b, C, c, D, d, E, e, F, f, G, g, H, h, I, i, J, j, K, k, L, l, M, m, N, n, O, o, P, p, Q, q, R, r, S, s, T, t, U, u, V, v, W, w, X, x, Y, y, Z ,z";
 
-  var pw = document.getElementById("password").value;
+const pw = document.getElementById("password").value;
 
-  // // check for an empty password
-  // if (pw == "") {
-  //   document.getElementById("message").innerHTML =
-  //     "Fill in the password please!";
-  //   return False;
-  // }
+if (pw.length < 8) {
+  alert("Password is not accepted");
+  return false;
 
-  if (pw.length < 8) {
-    console.log(pw);
-    alert("Password length must be a minimun 8 characters!");
-    
-  } else if (pw == !isNaN) {
-    console.log(pw);
-    alert("Password must contain one number!");
-    
-  } else if (pw == alphabet) {
-    console.log(pw);
-    alert("Password must contain one letter!");
+} else if (!pw.match(/[a-z]/g) && (/[A-Z]/g)) {
+  alert("Password is not accepted");
+  return false;
+ 
+} else if (!pw.match(/[0-9]/g)) {
+  alert("Password is not accepted");
+  return false;
 
-  } else {
-    console.log(pw);
-    alert("Password is correct!");
-    
-  }
+} else {
+  alert("Password is accepted");
+  return true;
 }
+}
+
