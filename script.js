c33982b31ddb37353730b3fd30f2703ae4c407fe
cbw29512 <divclass01@gmail.com>
2021-03-06 21:29:56 -0500

    minor changes to coding

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function checkPassword() {
   let pw = document.getElementById("password").value;
 
   if (usenam == "admin" && pw.length < 13 && !pw.match(/[!@#$%^&*]{3}/)) {
-    document.getElementById("message1").textContent="Admin password must be over 13 characters long and contain atleast 3 special characrters";
+    document.getElementById("message1").textContent="Admin password must be atleast 13 characters long and contain atleast 3 special characrters";
     document.getElementById('myform').reset();
     return false;
   
   } else if (usenam == "admin" && pw.length >= 13 && !pw.match(/[!@#$%^&*]{3}/)) {
-    document.getElementById("message2").textContent="Admin password must contain 3 special character";
+    document.getElementById("message2").textContent="Admin password must contain atleast 3 special character";
     document.getElementById('myform').reset();
     return false;
   
   } else if (usenam == "admin" && pw.length < 13 && pw.match(/[!@#$%^&*]{3}/)) {
-    document.getElementById("message3").textContent="Admin password must be over 13 characters long";
+    document.getElementById("message3").textContent="Admin password must be atleast 13 characters long";
     document.getElementById('myform').reset();
     return false;
     
@@ -23,12 +23,12 @@ function checkPassword() {
     return false;
 
   } else if (pw.length < 10 && !pw.match(/[A-Za-z]/g) && pw.match(/[0-9]/g)) {
-    document.getElementById("message4").textContent="Password must be over 10 characters and must include atleast one letter";
+    document.getElementById("message4").textContent="Password must be atleast 10 characters and must include atleast one letter";
     document.getElementById('myform').reset();
     return false;
   
   } else if (pw.length < 10 && pw.match(/[A-Za-z]/g) && !pw.match(/[0-9]/g)) {
-    document.getElementById("message5").textContent="Password must be over 10 characters and must include atleast one number";
+    document.getElementById("message5").textContent="Password must be atleast 10 characters and must include atleast one number";
     document.getElementById('myform').reset();
     return false;
 
@@ -37,8 +37,18 @@ function checkPassword() {
     document.getElementById('myform').reset();
     return false;
 
+  } else if (pw.length >= 10 && !pw.match(/[0-9]/g) && pw.match(/[A-Za-z]/g)) {
+    document.getElementById("message6").textContent="Password must include atleast one letter";
+    document.getElementById('myform').reset();
+    return false;
+
+  } else if (pw.length >= 10 && pw.match(/[0-9]/g) && !pw.match(/[A-Za-z]/g)) {
+    document.getElementById("message6").textContent="Password must include atleast one number";
+    document.getElementById('myform').reset();
+    return false;
+
   } else if (pw.length < 10) {
-    document.getElementById("message7").textContent="Password must be over 10 characters";
+    document.getElementById("message7").textContent="Password must be atleast 10 characters";
     document.getElementById('myform').reset();
     return false;
 
