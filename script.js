8340f87ad592fda864fae2c26e93eb9b8391c96b
cbw29512 <divclass01@gmail.com>
2021-03-06 19:32:32 -0500

    changed alerts to document.getElementById except password is correct, added spans in html

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
 function checkPassword() {
-  const adminpw = document.getElementById("username").value;
-  const pw = document.getElementById("password").value;
+  let adminpw = document.getElementById("username").value;
+  let pw = document.getElementById("password").value;
 
   if (adminpw == "admin" && pw.length < 13 && !pw.match(/[!@#$%^&*]{3}/)) {
-    alert(
-      "Admin password must be over 13 characters long and contain 3 special character"
-    );
+    document.getElementById("message1").textContent="Admin password must be over 13 characters long and contain atleast 3 special characrters";
     return false;
+  
   } else if (adminpw == "admin" && !pw.match(/[!@#$%^&*]{3}/)) {
-    alert("Admin password must contain 3 special character");
+    document.getElementById("message2").textContent="Admin password must contain 3 special character";
     return false;
+  
   } else if (adminpw == "admin" && pw.length < 13) {
-    alert("Admin password must be over 13 characters long");
+    document.getElementById("message3").textContent="Admin password must be over 13 characters long";
     return false;
+  
   } else if (pw.length < 10 && !pw.match(/[a-z]/g) && /[A-Z]/g) {
-    alert(
-      "Password must be over 10 characters and must include atleast one letter"
-    );
+    document.getElementById("message4").textContent="Password must be over 10 characters and must include atleast one letter";
     return false;
+  
   } else if (pw.length < 10 && !pw.match(/[0-9]/g)) {
-    alert(
-      "Password must be over 10 characters and must include atleast one number"
-    );
+    document.getElementById("message5").textContent="Password must be over 10 characters and must include atleast one number";
     return false;
+  
   } else if (!pw.match(/[a-z]/g) && /[A-Z]/g) {
-    alert("Password must include atleast one letter");
+    document.getElementById("message6").textContent="Admin password must contain 3 special character";
     return false;
+
   } else if (!pw.match(/[0-9]/g)) {
-    alert("Password must include atleast one number");
+    document.getElementById("message7").textContent="Password must include atleast one number";
     return false;
+
   } else if (!pw.match(/[0-9]/g) && !pw.match(/[a-z]/g) && /[A-Z]/g) {
-    alert("Password must include atleast one number and one number");
+    document.getElementById("message8").textContent="Password must include atleast one number and one number";
     return false;
+
   } else if (pw.length < 10) {
-    alert("Password must be over 10 characters");
+    document.getElementById("message9").textContent="Password must be over 10 characters";
     return false;
+
   } else {
     alert("Password is accepted");
     return true;
