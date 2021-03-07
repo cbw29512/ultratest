9e6bb2cc869d857a812fbfe623245bf0054fc62e
cbw29512 <divclass01@gmail.com>
2021-03-06 20:47:34 -0500

    changed adminpassword to usenam

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function checkPassword() {
-  let adminpw = document.getElementById("username").value;
+  let usenam = document.getElementById("username").value;
   let pw = document.getElementById("password").value;
 
-  if (adminpw != "admin" && pw.length < 13 && !pw.match(/[!@#$%^&*]{3}/)) {
+  if (usenam == "admin" && pw.length < 13 && !pw.match(/[!@#$%^&*]{3}/)) {
     document.getElementById("message1").textContent="Admin password must be over 13 characters long and contain atleast 3 special characrters";
     document.getElementById('myform').reset();
     return false;
   
-  } else if (adminpw != "admin" && !pw.match(/[!@#$%^&*]{3}/)) {
+  } else if (usenam == "admin" && !pw.match(/[!@#$%^&*]{3}/)) {
     document.getElementById("message2").textContent="Admin password must contain 3 special character";
     document.getElementById('myform').reset();
     return false;
   
-  } else if (adminpw != "admin" && pw.length < 13) {
+  } else if (usenam == "admin" && pw.length < 13) {
     document.getElementById("message3").textContent="Admin password must be over 13 characters long";
     document.getElementById('myform').reset();
     return false;
     
-  } else if (adminpw == "admin" && pw.match(/[!@#$%^&*]{3}/) && pw.match(/[A-Za-z]/g) && pw.match(/[0-9]/g)) {
+  } else if (usenam == "admin" && pw.match(/[!@#$%^&*]{3}/) && pw.match(/[A-Za-z]/g) && pw.match(/[0-9]/g)) {
     alert("Admin password is accepted");
     document.getElementById('myform').reset();
     return false;
