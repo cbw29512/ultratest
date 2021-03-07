db97f89ab92ba070a5234a4a94880c96a7feb5bb
cbw29512 <divclass01@gmail.com>
2021-03-06 22:17:13 -0500

    updated readme.md

--- script.js
+++ script.js
@@ -22,33 +22,33 @@ function checkPassword() {
     document.getElementById('myform').reset();
     return false;
 
+  } else if (pw.length < 10 && pw.match(/[A-Za-z]/g) && pw.match(/[0-9]/g)) {
+    document.getElementById("message4").textContent="Password must be atleast 10 characters";
+    document.getElementById('myform').reset();
+    return false;
+
   } else if (pw.length < 10 && !pw.match(/[A-Za-z]/g) && pw.match(/[0-9]/g)) {
-    document.getElementById("message4").textContent="Password must be atleast 10 characters and must include atleast one letter";
+    document.getElementById("message5").textContent="Password must be atleast 10 characters and must include atleast one letter";
     document.getElementById('myform').reset();
     return false;
   
   } else if (pw.length < 10 && pw.match(/[A-Za-z]/g) && !pw.match(/[0-9]/g)) {
-    document.getElementById("message5").textContent="Password must be atleast 10 characters and must include atleast one number";
+    document.getElementById("message6").textContent="Password must be atleast 10 characters and must include atleast one number";
     document.getElementById('myform').reset();
     return false;
 
   } else if (pw.length >= 10 && !pw.match(/[0-9]/g) && !pw.match(/[A-Za-z]/g)) {
-    document.getElementById("message6").textContent="Password must include atleast one number and one number";
+    document.getElementById("message7").textContent="Password must include atleast one number and one number";
     document.getElementById('myform').reset();
     return false;
 
   } else if (pw.length >= 10 && !pw.match(/[0-9]/g) && pw.match(/[A-Za-z]/g)) {
-    document.getElementById("message6").textContent="Password must include atleast one letter";
+    document.getElementById("message8").textContent="Password must include atleast one letter";
     document.getElementById('myform').reset();
     return false;
 
   } else if (pw.length >= 10 && pw.match(/[0-9]/g) && !pw.match(/[A-Za-z]/g)) {
-    document.getElementById("message6").textContent="Password must include atleast one number";
-    document.getElementById('myform').reset();
-    return false;
-
-  } else if (pw.length < 10) {
-    document.getElementById("message7").textContent="Password must be atleast 10 characters";
+    document.getElementById("message9").textContent="Password must include atleast one number";
     document.getElementById('myform').reset();
     return false;
 
@@ -60,8 +60,8 @@ function checkPassword() {
 
 function eraseText() {
   if (document.getElementsByTagName("span").value == "") {
-    return true;
+    return false;
   } else {
-    document.getElementById("output").value = "";
+    document.getElementsByTagName("span").empty();
   }
 }
